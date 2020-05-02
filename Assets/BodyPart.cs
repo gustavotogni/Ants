3f3758217c94801407da1624c522f439ed85a1bf
TOgni <gustavotogni@gmail.com>
2020-05-02 18:15:11 -0300

    Again

---
+++ Assets/BodyPart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public GameObject main;
+    public List<GameObject> bodyParts;
+    private List<Vector3> defaultBodyPartsOffset;
+    private Vector3 lastMainPosition; 
+    private Vector3 mainPositionDelta;
+
+    void Start()
+    {
+        main = bodyParts[0];
+        lastMainPosition = main.transform.position;
+        bodyParts.RemoveAt(0);
+        
+        foreach(var bodyPart in bodyParts) {
+
+            //Vector3.Distance(bodyPart.transform.position);
+            //defaultBodyPartsOffset.Add(
+        }              
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        
+        Vector3 currentMainPosition = main.transform.position;
+        mainPositionDelta = currentMainPosition - lastMainPosition;            
+
+        foreach(var bodyPart in bodyParts) {
+            bodyPart.transform.position += mainPositionDelta;
+        }
+        
+        lastMainPosition = currentMainPosition;
+    }
+}
