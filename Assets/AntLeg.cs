c845c397d4c9bdcc90e7e28e547524c0e45ad84c
TOgni <gustavotogni@gmail.com>
2020-05-02 18:17:00 -0300

    Nowyes

---
+++ Assets/AntLeg.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntLeg : MonoBehaviour
+{    
+    public GameObject femur;
+    public GameObject tibia;
+    public GameObject tarsus;
+    public GameObject claw;
+
+    private List<GameObject> bodyParts;
+
+    private List<Vector3> defaultBodyPartsOffset;
+    private Vector3 lastMainPosition; 
+    private Vector3 mainPositionDelta;
+
+    void Start()
+    {
+        bodyParts = new List<GameObject>();
+        bodyParts.Add(femur);
+        bodyParts.Add(tibia);
+        bodyParts.Add(tarsus);
+        //bodyParts.Add(claw);
+
+        lastMainPosition = claw.transform.position;
+        
+        
+        foreach(var bodyPart in bodyParts) {
+
+            //Vector3.Distance(bodyPart.transform.position);
+            //defaultBodyPartsOffset.Add(
+        }              
+    }
+
+    void Update()
+    {
+         //claw.transform.Translate(new Vector3(0.1f * Time.deltaTime, 0.0f, 0.0f));
+    }
+
+    void FixedUpdate() {
+        
+        Vector3 currentMainPosition = claw.transform.position;
+        mainPositionDelta = currentMainPosition - lastMainPosition;            
+
+        int i = 0;
+        foreach(var bodyPart in bodyParts) {
+            if (i <= 0) {
+                bodyPart.transform.position += mainPositionDelta;
+                i++;
+            } else {
+                break;
+            }            
+        }
+        
+        lastMainPosition = currentMainPosition;
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*
+
+
+    https://jeb.biologists.org/content/212/15/2426
+
+    */
+}
